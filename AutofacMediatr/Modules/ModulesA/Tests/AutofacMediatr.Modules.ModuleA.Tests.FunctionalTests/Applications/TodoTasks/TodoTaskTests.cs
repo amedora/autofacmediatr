c8e4a1f409fea7678916209796aaea57144fc447
Satoru Ohno <ono.satoru@tsuryo.co.jp>
2023-01-24 13:04:29 +0900

    tests

---
+++ AutofacMediatr/Modules/ModulesA/Tests/AutofacMediatr.Modules.ModuleA.Tests.FunctionalTests/Applications/TodoTasks/TodoTaskTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AutofacMediatr.Modules.ModuleA.Tests.FunctionalTests.Applications.TodoTasks
+{
+    public class TodoTaskTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
